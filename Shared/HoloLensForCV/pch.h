a76f27550940ce1550ab84d6ce5c663815c03c3f
Mitchell Doughty <doughtmw12@gmail.com>
2019-11-07 12:36:08 -0500

    Modified HoloLensForCV project to include the depthPvMapper sample, and integrate with Unity through ILC2PP WinRT.

--- Shared/HoloLensForCV/pch.h
+++ Shared/HoloLensForCV/pch.h
@@ -69,3 +69,11 @@
 #include "MediaFrameSourceGroup.h"
 
 #include "MultiFrameBuffer.h"
+
+// OpenCV includes (using Nuget package)
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+// Include the depth pv mapper class
+#include "DepthPvMapper.h"
